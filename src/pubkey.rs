5bef97d82c877407fc089c004395adc16a390512
PanosChtz <bomberb17@gmail.com>
2022-10-28 17:50:12 +0000

    Pubkey mining

--- src/pubkey.rs
+++ src/pubkey.rs
@@ -1,10 +1,18 @@
 use fastcrypto::traits::KeyPair;
 use rand::{rngs::StdRng, SeedableRng};
 use crate::secp256k1::Secp256k1KeyPair;
+use std::time::Instant;
+
+const FIXED_BYTES_NUM: usize = 3;
+
 
 pub fn pubkeymine() {
 
     let mut count: u64= rand::random();
+
+    let now = Instant::now();
+
+
 
   loop {
     count += 1;
@@ -13,15 +21,20 @@ pub fn pubkeymine() {
 
     let newkey = Secp256k1KeyPair::generate(&mut rng);
     
+    
+
     let newbytes = newkey.public().pubkey.serialize();
     
-    let fixedbytes = newbytes[1];
-
-    if fixedbytes == 100 {
-      println!("{}", count);
+    if &newbytes[1..=FIXED_BYTES_NUM] == vec![100; FIXED_BYTES_NUM] {
+      println!("{:?}", newbytes);
+      println!("{:?}", &newbytes[1..3]);
       break;
     }
   }
+
+
+  let elapsed = now.elapsed();
+  println!("Elapsed: {:.2?}", elapsed);
 
 
 }
